a2a7d576a93e667f549e68248d7488c963f37186
silasevinc <silasevinc23@gmail.com>
2024-07-21 18:41:27 +0300

    Update Program.cs

--- method/Program.cs
+++ method/Program.cs
@@ -1,4 +1,4 @@
-﻿static void sarkı_sozu()
+static void sarkı_sozu()
 {
     Console.WriteLine("Ben sigara dumanının altında\r\nYana yana en sonunda kül oldum\r\nSen kibritin hiç yanmayan ucunda\r\nBirinin hayatından geçmiş oldun");
 }
@@ -14,19 +14,27 @@ static int ikiye_bolumunden_kalan()
 }
 ikiye_bolumunden_kalan();
 //Aşağıdakinden kontrol edilebilir
-Console.WriteLine(ikiye_bolumunden_kalan());
+//Console.WriteLine(ikiye_bolumunden_kalan());
 
-
-static int iki_sayının_carpımı(int a,int b)
+Console.WriteLine("Lütfen ilk sayıyı giriniz");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Lütfen ikinci sayıyı giriniz");
+int b = Convert.ToInt32(Console.ReadLine());
+static int iki_sayının_carpımı(int sayi1,int sayi2)
 {
-
-    return a * b;
+    return sayi1 * sayi2;
 
 }
-iki_sayının_carpımı(4, 5);
-//Aşağıdakinden kontrol edilebilir
-Console.WriteLine(iki_sayının_carpımı(4, 5));
 
+iki_sayının_carpımı(a,b);
+//Aşağıdakinden kontrol edilebilir
+//Console.WriteLine(iki_sayının_carpımı(4, 5));
+
+
+Console.WriteLine("Lütfen isim giriniz");
+string isim = Console.ReadLine();
+Console.WriteLine("Lütfen soyisim giriniz");
+string soyisim = Console.ReadLine();
 static void isim_soyisim(string isim, string soyisim)
 {
 
@@ -34,4 +42,4 @@ static void isim_soyisim(string isim, string soyisim)
 
 
 }
-isim_soyisim("Sıla", "Sevinç");
+isim_soyisim(isim,soyisim);
